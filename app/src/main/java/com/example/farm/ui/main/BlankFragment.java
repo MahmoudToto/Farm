e68fa96c3575efc99c76b87da59e460ea1f93206
Toto <maahmed486@gmail.com>
2022-07-06 21:30:47 +0200

    initial commit

---
+++ app/src/main/java/com/example/farm/ui/main/BlankFragment.java
@@ -0,0 +1,107 @@
+package com.example.farm.ui.main;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.farm.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends DialogFragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_TITLE = "title";
+    private static final String ARG_MASSAGE = "massage";
+    private static final String ARG_ICON = "icon";
+
+
+    // TODO: Rename and change types of parameters
+    private String title;
+    private String massage;
+    private int icon;
+
+    public DialogFragment newInstance(String title, String massage, int icon) {
+
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_TITLE,title);
+        bundle.putString(ARG_MASSAGE,massage);
+        bundle.putInt(ARG_ICON,icon);
+        DialogFragment fragment = new DialogFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+
+    }
+
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment BlankFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static BlankFragment newInstance(String param1, String param2) {
+//        BlankFragment fragment = new BlankFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            title = getArguments().getString(ARG_TITLE);
+            massage = getArguments().getString(ARG_MASSAGE);
+            icon = getArguments().getInt(ARG_ICON);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage(massage);
+        builder.setIcon(icon);
+
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "Yes", Toast.LENGTH_SHORT).show();
+            }
+        });builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), "No", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return builder.create();
+    }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_blank, container, false);
+//    }
+}
---
+++ app/src/main/java/com/example/farm/ui/main/RecyclerInterface.java
@@ -0,0 +1,6 @@
+package com.example.farm.ui.main;
+
+public interface RecyclerInterface {
+    void onItemClick(int position);
+
+}
